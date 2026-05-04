70dd3dabd059f981056fe719c9e64f4bfc261afb
Arthur Detomi <arthurdetomi@gmail.com>
2026-05-04 10:00:03 -0300

    Remove duplication not buffer linked list

--- 2_linked_list/removeDups.ts
+++ 2_linked_list/removeDups.ts
@@ -19,6 +19,27 @@ function removeDups(list: NodeLinked) {
   }
 }
 
+// Don't use buffer
+
+// O(n²) time but O(1) space
+function removeDupsNotBuffer(list: NodeLinked) {
+  let currentNode: NodeLinked | null = list;
+
+  while (currentNode !== null) {
+    let runner = currentNode;
+
+    while (runner.next !== null && currentNode) {
+      if (currentNode && runner.next.data === currentNode.data) {
+        runner.next = runner.next.next;
+      } else {
+        runner = runner.next;
+      }
+    }
+
+    currentNode = currentNode.next;
+  }
+}
+
 let L: NodeLinked | null = new NodeLinked(1);
 
 L = L.appendToTail(2);
@@ -36,3 +57,23 @@ if (L) L.printList();
 removeDups(L);
 
 L.printList();
+
+console.log("Teste segunda função");
+
+let lista_2: NodeLinked | null = new NodeLinked(1);
+
+lista_2 = lista_2.appendToTail(2);
+
+lista_2 = lista_2.appendToTail(3);
+
+lista_2 = lista_2.appendToTail(1);
+
+lista_2 = lista_2.appendToTail(5);
+
+lista_2 = lista_2.appendToTail(2);
+
+if (lista_2) lista_2.printList();
+
+removeDupsNotBuffer(lista_2);
+
+lista_2.printList();

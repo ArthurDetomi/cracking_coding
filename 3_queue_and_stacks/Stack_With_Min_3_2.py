80fb66148c081fd1716b0aea509b12fa80352de5
Arthur Detomi <arthurdetomi@aluno.ufsj.edu.br>
2026-05-26 10:33:39 -0300

    Exercise 3.2 solved

--- 3_queue_and_stacks/Stack_With_Min_3_2.py
+++ 3_queue_and_stacks/Stack_With_Min_3_2.py
@@ -2,6 +2,9 @@ class StackNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+        self.min = 0
+        
+    
 class Stack:
     def __init__(self):
         self.head = None
@@ -13,10 +16,15 @@ class Stack:
         self.size += 1
         
         if self.head is None:
+            newNode.min = val
             self.head = newNode
             return True
         
         aux = self.head
+        
+        currentMinValue = min(aux.min, newNode.val)
+        
+        newNode.min = currentMinValue
         self.head = newNode
         self.head.next = aux
         
@@ -28,18 +36,33 @@ class Stack:
         
         val = self.head.val
         
+        print("Minimo = ", self.head.min)
+        
         self.size -= 1
             
         aux = self.head.next
     
         self.head = aux
         
+        
         return val
+    
+    def min(self):
+        return self.head.min
     
     def length(self):
         return self.size
     
-    def top(self):
-        if self.head is None:
-            raise IndexError("Stack is empty!")
-        return self.head.val
+stack = Stack()
+
+stack.push(6)
+stack.push(5)
+stack.push(8)
+stack.push(9)
+stack.push(1)
+stack.push(3)
+
+
+for _ in range(stack.length()):
+    print("Minimo atual = ", stack.min())
+    print(stack.pop())
--- 3_queue_and_stacks/Stack_ex3_2__sol2.py
+++ 3_queue_and_stacks/Stack_ex3_2__sol2.py
@@ -1,11 +1,10 @@
-class StackNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-class Stack:
+from Stack import Stack, StackNode
+            
+class StackWithMin:
     def __init__(self):
         self.head = None
         self.size = 0
+        self.auxStack = Stack()
     
     def push(self, val) -> bool:
         newNode = StackNode(val)
@@ -14,9 +13,16 @@ class Stack:
         
         if self.head is None:
             self.head = newNode
+            
+            self.auxStack.push(val)
+            
             return True
         
+        if val <= self.auxStack.top():
+            self.auxStack.push(val)
+        
         aux = self.head
+        
         self.head = newNode
         self.head.next = aux
         
@@ -28,13 +34,21 @@ class Stack:
         
         val = self.head.val
         
+        if val == self.auxStack.top():
+            self.auxStack.pop()
+        
         self.size -= 1
             
         aux = self.head.next
     
         self.head = aux
-        
+                
         return val
+    
+    def min(self):
+        if self.head is None:
+            raise IndexError("Stack is empty!")
+        return self.auxStack.top()
     
     def length(self):
         return self.size
@@ -43,3 +57,19 @@ class Stack:
         if self.head is None:
             raise IndexError("Stack is empty!")
         return self.head.val
+    
+stack = StackWithMin()
+
+stack.push(6)
+stack.push(5)
+stack.push(8)
+stack.push(9)
+stack.push(1)
+stack.push(3)
+stack.push(1)
+stack.push(1)
+stack.push(2)
+
+
+for _ in range(stack.length()):
+    print(f"minimo atual = {stack.min()}, valor do no = {stack.pop()}")
